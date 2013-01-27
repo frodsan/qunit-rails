c8821fde2791998ada129262dc39e39bec551a72
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-01-27 16:50:31 -0500

    remove .js extension from generated coffee file

--- lib/generators/qunit/install_generator.rb
+++ lib/generators/qunit/install_generator.rb
@@ -5,17 +5,17 @@ module Qunit
 
       desc 'Setup Qunit into test'
 
-      class_option :coffee, type: :boolean, aliases: "-c", default: false,
-                   desc: "Support for CoffeeScript"
+      class_option :coffee, type: :boolean, aliases: '-c', default: false,
+                   desc: 'Support for CoffeeScript'
 
       def create_setup_files
         empty_directory 'test/javascripts'
         empty_directory 'test/stylesheets'
 
         if !options[:coffee]
-          template 'test_helper.js',  'test/javascripts/test_helper.js'
+          template 'test_helper.js', 'test/javascripts/test_helper.js'
         else
-          template 'test_helper.js.coffee', 'test/javascripts/test_helper.js.coffee'
+          template 'test_helper.coffee', 'test/javascripts/test_helper.coffee'
         end
 
         template 'test_helper.css', 'test/stylesheets/test_helper.css'
