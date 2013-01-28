57ff8184bbb8c961d9c6e45fffce3a67f25ad750
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-01-27 19:54:22 -0500

    single quotes > double ones

--- config/initializers/qunit-rails.rb
+++ config/initializers/qunit-rails.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.paths << Rails.root.join("test", "javascripts") << Rails.root.join("test", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join('test', 'javascripts') << Rails.root.join('test', 'stylesheets')
