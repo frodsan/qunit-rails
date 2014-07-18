bfd3e8904f9cd995bf75f54d95d8e1bb59dcf16d
Nadav Shatz <nadavshatz@gmail.com>
2014-07-18 11:40:34 +0300

    Added support for the option to clear the session on each load

--- app/controllers/q_unit/rails/test_controller.rb
+++ app/controllers/q_unit/rails/test_controller.rb
@@ -1,7 +1,15 @@
 module QUnit
   module Rails
     class TestController < ActionController::Base
+      after_action :clear_session
+
       layout false
+
+      private
+
+      def clear_session
+        reset_session if Rails.application.config.qunit.reset_session
+      end
     end
   end
 end
